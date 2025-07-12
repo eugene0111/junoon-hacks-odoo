bcb32c2614c3970c1d3a41b451b523c29b835d81
Khushi <khushig3011@gmail.com>
2025-07-12 11:38:53 +0530

    Public profile view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import {
 } from "react-router-dom";
 import UserProfile from "./pages/UserProfile";
 import AuthFrontend from "./pages/Auth";
+import PublicProfileView from "./pages/PublicProfileView";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/user-profile" element={<UserProfile />} />
           <Route path="/auth" element={<AuthFrontend />} />
+          <Route path="/public-profile" element={<PublicProfileView />} />
         </Routes>
       </BrowserRouter>
     </>
